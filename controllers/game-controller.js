d1543ada7503a78a444a0422e95105f647953df8
Giancarlo Paredes <Giancarlo Paredes>
2016-04-20 18:00:24 -0700

    improve formatting

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -18,87 +18,83 @@ module.exports = function(app, server) {
     }
 
     io.sockets.on('connection', function(socket) {
-      console.log(grid);
-      
-      socket.on('client_connected', function(player) {
-        player.id = socket.id;
-        player.mark = xo;
-        
-        if(xo == 'x' && o == false) 
-        {
-          xo = 'o';
-          o = true;
-        }
-        else
-        {
-          xo = 'spectator';
-        }
-        m_players[i] = player;
-        i++;
-        
-        socket.emit('connect_1', player);
-        //socket.emit('draw_board', board);
-        io.sockets.emit('load',m_players);
-    });
-      
-    socket.on('process_move', function(coords, player) {
-        var n = 0;
-        coords = coords.replace("#",'');
-        grid[coords] = player.mark;
-        
-        console.log(grid);
-        io.sockets.emit('mark', coords);
-        
-        // Win check
-        if( (grid['0-0'] == grid['0-1'] && grid['0-1'] == grid['0-2'] && grid['0-0'] != '') || 
-        (grid['1-0'] == grid['1-1'] && grid['1-1'] == grid['1-2'] && grid['1-0'] != '') ||
-        (grid['2-0'] == grid['2-1'] && grid['2-1'] == grid['2-2'] && grid['2-0'] != '') ||
-        
-        (grid['0-0'] == grid['1-0'] && grid['1-0'] == grid['2-0'] && grid['0-0'] != '') ||
-        (grid['0-1'] == grid['1-1'] && grid['1-1'] == grid['2-1'] && grid['0-1'] != '') ||
-        (grid['0-2'] == grid['1-2'] && grid['1-2'] == grid['2-2'] && grid['0-2'] != '') ||
-        
-        (grid['0-0'] == grid['1-1'] && grid['1-1'] == grid['2-2'] && grid['0-0'] != '') ||
-        (grid['2-0'] == grid['1-1'] && grid['1-1'] == grid['0-2'] && grid['2-0'] != '') 
-        )
-        {
-          io.sockets.emit('gameover', xo);
-        }
-    });
-      
-    socket.on('disconnect', function() {
-         var j = 0;
-         var n = 0;
-         var tmp = [];
 
-         while (n < m_players.length)
-         {
-           if (m_players[j].id == socket.id)
-           {
-             if(m_players[j].mark == 'o')
-             {
-               xo = 'o';
-               o = false;
-             }
-             if(m_players[j].mark == 'x')
-             {
-               xo = 'x';
-             }
-         	   n++;
-         	 }
-         	 
-         	 if (n < m_players.length)
-         	 {
-         	   tmp[j] = m_players[n];
-         	   j++;
-         	   n++;
-         	  }
-         	}
-         	
-         	m_players = tmp;
-         	i = j;
-          io.sockets.emit('load', m_players);
-       });
+      	socket.on('client_connected', function(player) {
+        	player.id = socket.id;
+        	player.mark = xo;
+        
+        	if(xo == 'x' && o == false)  {
+          		xo = 'o';
+          		o = true;
+        	} else
+				xo = 'spectator';
+
+	        m_players[i] = player;
+	        i++;
+        
+	        socket.emit('connect_1', player);
+	        io.sockets.emit('load',m_players);
+    	});
+      
+    	socket.on('process_move', function(coords, player) {
+	        var n = 0;
+	        coords = coords.replace("#",'');
+	        grid[coords] = player.mark;
+        
+	        console.log(grid);
+	        io.sockets.emit('mark', coords);
+        
+	        if ( 
+	        	(grid['0-0'] == grid['0-1'] && grid['0-1'] == grid['0-2'] && grid['0-0'] != '') ||
+
+	        	(grid['1-0'] == grid['1-1'] && grid['1-1'] == grid['1-2'] && grid['1-0'] != '') ||
+
+	        	(grid['2-0'] == grid['2-1'] && grid['2-1'] == grid['2-2'] && grid['2-0'] != '') ||
+	        
+		        (grid['0-0'] == grid['1-0'] && grid['1-0'] == grid['2-0'] && grid['0-0'] != '') ||
+
+		        (grid['0-1'] == grid['1-1'] && grid['1-1'] == grid['2-1'] && grid['0-1'] != '') ||
+
+		        (grid['0-2'] == grid['1-2'] && grid['1-2'] == grid['2-2'] && grid['0-2'] != '') ||
+	        
+		        (grid['0-0'] == grid['1-1'] && grid['1-1'] == grid['2-2'] && grid['0-0'] != '') ||
+
+		        (grid['2-0'] == grid['1-1'] && grid['1-1'] == grid['0-2'] && grid['2-0'] != '') 
+	        )
+	        {
+	          io.sockets.emit('gameover', xo);
+	        }
+    	});
+      
+	    socket.on('disconnect', function() {
+	        var j = 0;
+	        var n = 0;
+	        var tmp = [];
+
+	        while (n < m_players.length) {
+	        	if (m_players[j].id == socket.id) {
+	            	if(m_players[j].mark == 'o') {
+	               		xo = 'o';
+	               		o = false;
+	             	}
+	             
+	            	if(m_players[j].mark == 'x')
+	            		xo = 'x';
+	         	  
+	         		n++;
+	         	}
+	         	 
+	         	if (n < m_players.length) {
+	         		tmp[j] = m_players[n];
+	         	   	j++;
+	         	   	n++;
+	         	}
+	        }
+	         	
+	        m_players = tmp;
+	        i = j;
+	        io.sockets.emit('load', m_players);
+	    });
 	});
 
 	app.get('/', function(req, res){
